ae980166e136788d85111d0c894ba559d38c6ab8
raphael-dev <raphael.ikediashi@betastore.co>
2022-06-17 11:06:58 +0100

    Adding to cart

--- src/app/pages/Catalog/components/ProductItems.tsx
+++ src/app/pages/Catalog/components/ProductItems.tsx
@@ -5,53 +5,85 @@ import { CardBody } from 'reactstrap';
 import styled from 'styled-components';
 import { customMedia } from 'styles/media';
 
-const Sale = `${process.env.PUBLIC_URL}/img/for_store/catalog/sale_lable.png`;
-const New = `${process.env.PUBLIC_URL}/img/for_store/catalog/new_lable.png`;
+interface Catalog {
+  name: string;
+  itemId: string;
+  salesPrice: number;
+  itemCount: any;
+}
 
+let StoreCatalogItems: Catalog[] = [];
+
+let ItemCount = StoreCatalogItems.length;
+
+const AddToCart = (num, item) => {
+  for (var i = 0; i < StoreCatalogItems.length; i++) {
+    if (StoreCatalogItems[i].itemId === item.ItemId) {
+      ItemCount = i;
+      break;
+    }
+  }
+
+
+  StoreCatalogItems[ItemCount] = {
+    name: item.Name,
+    itemId: item.ItemId,
+    salesPrice: item.SalesPrice,
+    itemCount: num,
+  };
+
+  alert(ItemCount + ' - ' + item.Name);
+};
+
+const SubFromCart = (num, item) => {
+  StoreCatalogItems[ItemCount] = {
+    name: item.Name,
+    itemId: item.ItemId,
+    salesPrice: item.SalesPrice,
+    itemCount: item.num,
+  };
+
+  alert(ItemCount + ' - ' + item.Name);
+};
 const Catalog = ({ items }) => (
   <CatalogItemsWrap>
     <CatalogItems>
       {items.map(item => (
         <CatalogItem key={item.i}>
-          <CatalogItemLink to="/e-commerce/product_page">
+          <CatalogItemLink>
             <CatalogItemImageWrap>
-              <CatalogItemImg src={`https://test.api.simplemarket.app/api${item.ImageLink}`} alt="catalog-item-img" />
+              <CatalogItemImg
+                src={`https://test.api.simplemarket.app/api${item.ImageLink}`}
+                alt="catalog-item-img"
+              />{' '}
             </CatalogItemImageWrap>
             <CatalogItemInfo>
               <CatalogItemTitle>{item.Name}</CatalogItemTitle>
-              <CatalogItemPrice>${item.SalesPrice}</CatalogItemPrice>
+              <CatalogItemDescription>
+                {item.description}
+              </CatalogItemDescription>
+              <CatalogItemPrice>
+                Unit Price : ₦ {item.SalesPrice}
+              </CatalogItemPrice>
             </CatalogItemInfo>
+            <div>
+              <SearchBtn onClick={() => AddToCart(1, item)}>Add +1</SearchBtn>
+              <SearchBtn onClick={() => AddToCart(3, item)}>Add +3</SearchBtn>
+              <SearchBtn onClick={() => SubFromCart(1, item)}>
+                Reduce -1
+              </SearchBtn>
+            </div>
+            <div>
+              <Quantity>
+                Quantiy : <TextAfterBtn>40</TextAfterBtn>
+              </Quantity>
+            </div>
           </CatalogItemLink>
-          <div>
-            <SearchBtn>Add to Cart</SearchBtn>
-          </div>
         </CatalogItem>
       ))}
     </CatalogItems>
   </CatalogItemsWrap>
 );
-const SearchBtn = styled(Button)`
-  margin-top: -20px;
-  margin-left: 40px;
-  height: 40px;
-  padding-left: 40px;
-  padding-right: 40px;
-  color: #ed017f;
-  border-color: #ed017f;
-
-  ${customMedia.lessThan('small')`
-    height: 29px;
-  `};
-
-  &:hover {
-    background-color: #e4811c;
-    color: #fff;
-  }
-  &:after {
-    background-color: #e4811c;
-    color: #fff;
-  }
-`;
 
 const CatalogItemsWrap = styled.div`
   overflow: hidden;
@@ -73,7 +105,7 @@ const CatalogItems = styled.div`
 const CatalogItem = styled(CardBody)`
   width: calc(33.3333% - 30px);
   margin-bottom: 30px;
-  height: 440px;
+  height: 460px;
   position: relative;
   background-color: #fff;
   margin-right: 30px;
@@ -85,8 +117,8 @@ const CatalogItem = styled(CardBody)`
   box-shadow: none;
 `;
 
-const CatalogItemLink = styled(Link)`
-  padding: 40px 30px;
+const CatalogItemLink = styled.div`
+  padding: 40px 15px;
   display: block;
   text-decoration: none;
   color: #3ea3fc;
@@ -119,8 +151,9 @@ const CatalogItemImg = styled.img`
 const CatalogItemInfo = styled.div`
   text-align: left;
   position: relative;
-  width: calc(100% - 90px);
+  /* width: calc(100% - 90px); */
   display: block;
+  margin-bottom: 10px;
 `;
 
 const CatalogItemTitle = styled.h4`
@@ -128,11 +161,13 @@ const CatalogItemTitle = styled.h4`
   color: #646777;
   font-weight: 600;
   transition: all 0.3s;
-  font-size: 16px;
+  font-size: 10px;
   line-height: 24px;
   margin-top: 0;
   margin-bottom: 0;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  text-align: center;
+  border-bottom: 1px solid rgb(218, 218, 218);
 
   &:hover {
     color: #4ce1b6;
@@ -142,41 +177,74 @@ const CatalogItemTitle = styled.h4`
 const CatalogItemDescription = styled.p`
   margin: 0;
   color: #646777;
-  line-height: 17px;
+  line-height: 12px;
   font-weight: 400;
 `;
 
 const CatalogItemPrice = styled.h1`
-  right: -90px;
   color: #646777;
-  position: absolute;
-  top: 0;
+  /* position: absolute; */
+  /* top: 0; */
+  text-align: center;
   line-height: 36px;
-  font-size: 20px;
+  font-size: 12px;
   margin-top: 0;
   font-weight: 400;
   margin-bottom: 0;
+  border: 1px solid rgb(218, 218, 218);
 `;
 
 const CatalogItemOldPrice = styled.p`
-    position: absolute;
-    top: 36px;
-    line-height: 28px;
-    color: #999;
-    margin: 0;
-    text-decoration: line-through;
-    color: #646777;
-    right: -90px;
-}
+  position: absolute;
+  top: 36px;
+  line-height: 28px;
+  color: #999;
+  margin: 0;
+  text-decoration: line-through;
+  color: #646777;
+  right: -90px;
 `;
 
-const CatalogItemColor = styled.h1`
-  height: 10px;
-  width: 10px;
-  margin-right: 8px;
-  display: inline-block;
-  border-radius: 50%;
+const SearchBtn = styled.span`
+  margin-left: 5px;
+  height: 30px;
+  padding: 10px;
+  cursor: pointer;
+  color: #ffffff;
+  border-radius: 3px;
+  background-color: #100276;
+
+  ${customMedia.lessThan('small')`
+    height: 29px;
+  `};
+
+  &:hover {
+    background-color: rgb(5, 183, 203);
+    color: #fff;
+  }
+  &:after {
+    background-color: #e4811c;
+    color: #fff;
+  }
+`;
+
+const Quantity = styled.div`
   margin-top: 10px;
+  margin-bottom: 10px;
+  text-align: center;
+  height: 30px;
+  padding: 10px 10px 0px 10px;
+  border-radius: 5px;
+`;
+
+const TextAfterBtn = styled.span`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  width: 100%;
+  height: 30px;
+  padding: 10px 10px 10px 10px;
+  border: 1px solid rgb(218, 218, 218);
+  border-radius: 5px;
 `;
 
 export default Catalog;
